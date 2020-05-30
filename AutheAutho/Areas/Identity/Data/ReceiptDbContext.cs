5fabe222b5eb58568a0cd5e566c69fc825973ec7
Okaks <bokakwu@email.com>
2020-05-30 08:58:09 +0100

    Task

---
+++ AutheAutho/Areas/Identity/Data/ReceiptDbContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutheAutho.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AutheAutho.Data
+{
+    public class ReceiptDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ReceiptDbContext(DbContextOptions<ReceiptDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ AutheAutho/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using AutheAutho.Areas.Identity.Data;
+using AutheAutho.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(AutheAutho.Areas.Identity.IdentityHostingStartup))]
+namespace AutheAutho.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<ReceiptDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("ReceiptDbContextConnection")));
+
+                services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<ReceiptDbContext>();
+            });
+        }
+    }
+}
